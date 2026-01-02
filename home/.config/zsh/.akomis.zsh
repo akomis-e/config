19c535c565e6503e73bd812a3d6a1fe4a9194cdf
akomis <greyakomis@gmail.com>
2026-01-02 17:23:37 +0200

    missed zsh dot files

---
+++ home/.config/zsh/.akomis.zsh
@@ -0,0 +1,35 @@
+
+
+# alias la="ls -lah --color=auto --group-directories-first"
+# alias la="rgrc ls -lA --group-directories-first"
+alias la="eza -al --group-directories-first"
+alias man="batman_p"
+alias cat="bat -p"
+
+
+alias cdc="cd ~/.config"
+
+## MY GIT ALIASES
+alias gits="git status"
+
+
+alias p="print"
+
+## Batman
+export BAT_THEME=gruvbox-dark
+
+batman_p() {
+    #do things with parameters like $1 such as
+    if (( $# == 2 ))
+	then
+		/usr/bin/man $1 $2 | bat -pl manpage
+    elif (( $# == 1 ))
+	then
+		/usr/bin/man $1 | bat -pl manpage
+	else
+		echo 'batman: expected 1 or 2 parameters to man'
+	fi
+}
+
+
+# ls -AFoqv --color --group-directories-first
